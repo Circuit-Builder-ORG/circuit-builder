3cd57fbf43a73a7ba30933f2ece207fe43d76516
Hisokahazza <147928782+Hisokahazza@users.noreply.github.com>
2024-12-07 14:55:56 +0000

    Added basic websocket boilerplate

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ const app = express();
 const PORT = 3000;
 
 // Setup DB
-// Load environment variables
 const dotenv = require('dotenv').config();
-
 const mongoose = require('mongoose');
 const mongoURI = process.env.MONGOURI;
 
@@ -13,11 +11,26 @@ mongoose.connect(mongoURI)
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('MongoDB connection error:', err));
 
-// Start server
+// Setup WebSocket
+const http = require('http');
+const socketIo = require('socket.io');
+
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
+
+// Express routes
 app.get('/', (req, res) => {
     res.send('Circuit Builder API is running!');
 });
 
-app.listen(PORT, () => {
+// Start the server (single listen call)
+server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
